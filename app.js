ef52d8dc9d7dde40a317affa0f14fa8d65eb2e04
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-04-05 17:38:17 +0200

    Zone fight - stats

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("coucou");
+const $ = require('jquery');
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,16 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           use: ['css-loader', 'sass-loader'],
         })
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
