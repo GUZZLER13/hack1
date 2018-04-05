837fec95c2003ceff7bd24063e3fbac5300c01f3
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-04-05 22:03:27 +0200

    Fightzone - add dynamic fighter + button fight +defeat

--- app.js
+++ app.js
@@ -1,16 +1,76 @@
 const $ = require('jquery');
+const GET_SUPERHERO_INFOS = (id) => `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${id}.json`;
 
 $(document).ready(function() {
-  // Test avatar
-  $('.avatar').css('background-image', 'url(https://raw.githubusercontent.com/akabab/superhero-api/0.2.0/api/images/sm/1-a-bomb.jpg)');
+  const $btnFight = $('.button-fight');
 
-  //Test levels
-  $('.fill-level', '.fighter').each(function() {
-    $(this).css("width", `${$(this).parent('.level').data("level")}%`);
-  });
+  const initFighter = (_fighter, _zone) => {
+    const $zone = $(`.${_zone}`);
+    const $fighter = $('.fighter', $zone);
+    const $stats = $('.stats', $fighter);
+    const $avatar = $('.avatar', $fighter);
 
-  //Test levels
-  $('.fill-life', '.fightzone').each(function() {
-    $(this).css("width", `${$(this).parent('.lifepoints').data("life")}%`);
+    $fighter.data("life", 100);
+    $fighter.data("power", _fighter.powerstats.power);
+    $fighter.data("defense", _fighter.powerstats.strength);
+    $fighter.data("speed", _fighter.powerstats.speed);
+
+    // Fill Stats
+    $('.strength .fill-level', $stats).css("width", `${_fighter.powerstats.strength}%`);
+    $('.power .fill-level', $stats).css("width", `${_fighter.powerstats.power}%`);
+    $('.speed .fill-level', $stats).css("width", `${_fighter.powerstats.speed}%`);
+
+    $('.name-avatar', $avatar).text(_fighter.name);
+    $('.img-avatar', $avatar).css('background-image', `url(${_fighter.images.sm})`);
+
+    $('.fill-life', $zone).css("width", `100%`);
+  }
+
+  const initFightzone = () => {
+    // Get the opponent informations
+    $.get(GET_SUPERHERO_INFOS(1), (data) => {
+      initFighter(data, 'opponent');
+
+      // Get the player informations
+      $.get(GET_SUPERHERO_INFOS(2), (data) => {
+        initFighter(data, 'player');
+        $btnFight.show();
+      });
+    });
+  }
+
+  const fight = (_src, _dest) => {
+    const $zoneSrc = $(`.${_src}`);
+    const $fighterSrc = $('.fighter', $zoneSrc);
+    const $zoneDest = $(`.${_dest}`);
+    const $fighterDest = $('.fighter', $zoneDest);
+
+    const srcPower = $fighterSrc.data("power");
+
+    const destDef = $fighterDest.data("defense");
+    const destLife = $fighterDest.data("life");
+    const destSpeed = $fighterDest.data("speed");
+
+    // How to - do damages on dest
+    // destSpeed chances to avoid attack
+    if (Math.round(Math.random() * 100) >= destSpeed) {
+      const life = destLife - 5;
+
+      $fighterDest.data("life", life);
+      $('.fill-life', $zoneDest).css("width", `${life}%`);
+
+      if (life <= 0) {
+        $btnFight.hide();
+        $('.defeat', $zoneDest).fadeIn();
+      }
+    }
+  }
+
+  initFightzone();
+
+  $btnFight.on("click", function() {
+    // The player attack the opponent
+    fight('player', 'opponent');
+    fight('opponent', 'player');
   });
 });
